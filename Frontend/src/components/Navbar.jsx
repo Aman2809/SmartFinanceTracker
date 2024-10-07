ba9594bb90dd6867d0ce43123e66d73326aeea04
Aman2809 <jhaaman1005@gmail.com>
2024-10-08 00:20:48 +0530

    Changed the Port from Http to Https.

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -6,70 +6,70 @@ import Financify2 from '../img/Financify2.png'
 
 function Navbar({ openLoginModal }) {
 
-let navigate=useNavigate()
-const [login, setLogin] = useState(false)
-const [user,setUser]=useState(undefined)
+    let navigate = useNavigate()
+    const [login, setLogin] = useState(false)
+    const [user, setUser] = useState(undefined)
 
 
 
 
 
-useEffect(() => {
+    useEffect(() => {
 
-setLogin(isLoggedIn())
-setUser(getCurrentUser())
+        setLogin(isLoggedIn())
+        setUser(getCurrentUser())
 
-}, [login])
+    }, [login])
 
-const logout=()=>{
-  doLogout(()=>{
-    setLogin(false)
-    navigate("/")
-    
-  })
-}
+    const logout = () => {
+        doLogout(() => {
+            setLogin(false)
+            navigate("/")
 
-
+        })
+    }
 
 
-  return (
-    <nav className="flex justify-between items-center px-20 py-4 bg-transparent absolute top-0 left-0 w-full z-10">
-      <div className="flex items-center space-x-1">
-        <img src={Financify2} alt="Origin Logo" className="h-16 w-16" />
-        <h2 className="text-2xl font-bold text-black">Financfy</h2>
-      </div>
-      <ul className="flex space-x-8 text-black">
-        <NavLink to="/" className="hover:text-lg font-bold transition-all duration-300"><li>Home</li></NavLink>
-        <NavLink to="/user/dashboard" className="hover:text-lg font-bold transition-all duration-300"><li>Dashboard</li></NavLink>
-        <NavLink to="/about" className="hover:text-lg font-bold transition-all duration-300"><li>About</li></NavLink>
-        <NavLink to="/faq" className="hover:text-lg font-bold transition-all duration-300"><li>FAQ</li></NavLink>
-      </ul>
-      <div className="flex space-x-2">
 
-        {
-          login &&(
-            <>
-            
-            <button onClick={logout} className="px-4 py-2 text-black font-bold rounded hover:text-black">Logout</button>
-            <button className="px-4 py-2 text-black font-bold rounded bg-pink-300 hover:bg-pink-400">Get Started</button>
-            
-            </>
-          )
-        }
-        
-        {
-          !login &&(
 
-            <>
-            <button onClick={openLoginModal} className="px-4 py-2 text-black font-bold rounded hover:text-black">Log in</button>
-            <button className="px-4 py-2 text-black font-bold rounded bg-pink-300 hover:bg-pink-400">Get Started</button>
-            </>
-          )
-        }
+    return (
+        <nav className="flex justify-between items-center px-20 py-4 bg-transparent absolute top-0 left-0 w-full z-10">
+            <div className="flex items-center space-x-1">
+                <img src={Financify2} alt="Origin Logo" className="h-16 w-16" />
+                <h2 className="text-2xl font-bold text-black">Financfy</h2>
+            </div>
+            <ul className="flex space-x-8 text-black">
+                <NavLink to="/" className="hover:text-lg font-bold transition-all duration-300"><li>Home</li></NavLink>
+                <NavLink to="/user/dashboard" className="hover:text-lg font-bold transition-all duration-300"><li>Dashboard</li></NavLink>
+                <NavLink to="/about" className="hover:text-lg font-bold transition-all duration-300"><li>About</li></NavLink>
+                <NavLink to="/faq" className="hover:text-lg font-bold transition-all duration-300"><li>FAQ</li></NavLink>
+            </ul>
+            <div className="flex space-x-2">
 
-      </div>
-    </nav>
-  );
+                {
+                    login && (
+                        <>
+
+                            <button onClick={logout} className="px-4 py-2 text-black font-bold rounded hover:text-black">Logout</button>
+                            <button className="px-4 py-2 text-black font-bold rounded bg-pink-300 hover:bg-pink-400">Get Started</button>
+
+                        </>
+                    )
+                }
+
+                {
+                    !login && (
+
+                        <>
+                            <button onClick={openLoginModal} className="px-4 py-2 text-black font-bold rounded hover:text-black">Log in</button>
+                            <button className="px-4 py-2 text-black font-bold rounded bg-pink-300 hover:bg-pink-400">Get Started</button>
+                        </>
+                    )
+                }
+
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
